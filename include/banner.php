45e6cad58ea6449a7e1489f4e030b5ef0491a700
Arbind-69 <arbinddash01@gmail.com>
2025-01-19 00:17:36 -0700

    this is first push

---
+++ include/banner.php
@@ -0,0 +1,19 @@
+<div class="banner">
+    <div class="container">
+        <div class="center-content">
+            <h2>
+                Welcome!
+            </h2>
+            <h1>
+                I am <span>web Designer</span>
+            </h1>
+            <div class="button-tag">
+                <button class="button1">View work</button>
+                <button class="button2">Hire Me!</button>
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
---
+++ include/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arbind -portfolio</title>
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/common.css">
+    <link rel="stylesheet" href="./css/about.css">
+    <link rel="stylesheet" href="./css/blog.css">
+    <link rel="stylesheet" href="./css/contact.css">
+    <link rel="stylesheet" href="./css/portfolio.css">
+    <link rel="stylesheet" href="./css/services.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <link rel="stylesheet" href="./css/banner.css">
+    <link rel="stylesheet" href="./css/header.css">
+
+    
+
+
+
+
+</head>
+<body>
+    <header>
+        <div class="container">
+        <div class="navBar">
+
+        <div class="logo">
+            <img src="./img/logo.png" alt="">
+
+        </div>
+        
+        <ul class="nav">
+            <li class="nav-link"><a href="">Home</a></li>
+            <li class="nav-link"><a href="">About</a></li>
+            <li class="nav-link"><a href="">Services</a></li>
+            <li class="nav-link"><a href="">Portfolio</a></li>
+            <li class="nav-link"><a href="">Blog</a></li>
+            <li class="nav-link"><a href="">Contact</a></li>
+           </ul>
+
+
+       
+
+
+        </div>
+        </div>
+       
+
+        
+
+       
+    
+    </header>
+          
+
+
+
+
+ 
+ 
+
+
+
+
+
+    
+
+    
+        
+    
